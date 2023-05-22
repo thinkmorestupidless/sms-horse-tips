0c4eefcd4b4b145b07c88187f725c6e5b34b2ad4
thinkmorestupidless <trevor@thinkmorestupidless.com>
2023-05-22 22:51:01 +0100

    put csrf back

--- config.py
+++ config.py
@@ -7,10 +7,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
     TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
     TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
-    SERVER_NAME = '127.0.0.1:8080'
 
 class ProductionConfig(Config):
-    SERVER_NAME = 'sms-horse-tips.herokuapp.com'
+    pass
 
 class StagingConfig(Config):
     DEBUG = True
